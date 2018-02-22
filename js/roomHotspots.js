7ff1a6f44cba9cbb23a746357a523177c0e5b721
Giuseppe Vit <giuseppevit@gmail.com>
2018-02-22 23:20:23 +0100

    add raycaster and infowindow popup

--- js/roomHotspots.js
+++ js/roomHotspots.js
@@ -3,8 +3,10 @@ var camera, scene, renderer;
 var controls;
 
 var mouse;
+var raycaster;
+var INTERSECTED;
 
-var circle1, circl2, circle3;
+var circle1, circle2, circle3;
 
 var texture;
 
@@ -87,6 +89,7 @@ function init () {
   // circle1.userData.infoWindowHeight = 500;
   scene.add(circle3);
   
+  raycaster = new THREE.Raycaster();
   renderer = new THREE.WebGLRenderer();
 
   renderer.setPixelRatio(window.devicePixelRatio);
@@ -106,6 +109,83 @@ function animate () {
 
 function render () {
   camera.updateMatrixWorld();
+
+  // update the picking ray with the camera and mouse position
+  raycaster.setFromCamera( mouse, camera );
+
+  // calculate objects intersecting the picking ray
+  // (Intersections are returned sorted by distance, closest first)
+  var intersects = raycaster.intersectObjects(scene.children);
+
+  // Leave only hotspots and remove any other kind of object intersected
+  var i = intersects.length;
+  while (i--) {
+    if (intersects[i].object.userData.kind !== 'hotspot') {
+      intersects.splice(i, 1);
+    }
+  }
+
+  if (intersects.length > 0) {
+    if (INTERSECTED !== intersects[0].object) {
+      if (INTERSECTED) {
+        INTERSECTED.material.transparent = true;
+        INTERSECTED.material.opacity = 0.6;
+      }
+
+      var infowindow = document.getElementById('infowindow');
+      infowindow.style.width = intersects[0].object.userData.infoWindowWidth + 'px' || '100px';
+      infowindow.style.height = intersects[0].object.userData.infoWindowHeight + 'px' || '25px';
+      camera.updateMatrixWorld();
+      var proj = createVector(intersects[0].object.position.x, intersects[0].object.position.y, intersects[0].object.position.z, camera, window.innerWidth, window.innerHeight);
+      infowindow.style.left = proj.x + 'px';
+      infowindow.style.top = proj.y + 'px';
+
+      // right side overflow
+      if ((proj.x + infowindow.offsetWidth) > window.innerWidth) {
+        proj.x = window.innerWidth - infowindow.offsetWidth;
+        proj.x -= 5;
+        infowindow.style.left = proj.x + 'px';
+      }
+
+      // left side overflow
+      if (proj.x < 0) {
+        proj.x = 0;
+        proj.x += 5;
+        infowindow.style.left = proj.x + 'px';
+      }
+
+      // bottom overflow
+      if ((proj.y + infowindow.offsetHeight) > window.innerHeight) {
+        proj.y = window.innerHeight - infowindow.offsetHeight;
+        proj.y -= 5;
+        infowindow.style.top = proj.y + 'px';
+      }
+
+      // top overflow
+      if (proj.y < 0) {
+        proj.y = 0;
+        proj.y += 5;
+        infowindow.style.top = proj.y + 'px';
+      }
+
+      infowindow.innerHTML = intersects[0].object.userData.HTMLDescription;
+      infowindow.style.visibility = 'visible';
+
+      INTERSECTED = intersects[0].object;
+      INTERSECTED.material.transparent = false;
+      INTERSECTED.material.opacity = 1;
+    }
+  } else {
+    if (INTERSECTED) {
+      INTERSECTED.material.transparent = true;
+      INTERSECTED.material.opacity = 0.6;
+    }
+    var infowindow = document.getElementById('infowindow');
+    infowindow.innerHTML = 'Description';
+    infowindow.style.visibility = 'hidden';
+    INTERSECTED = null;
+  }
+
   renderer.render(scene, camera);
 }
 
