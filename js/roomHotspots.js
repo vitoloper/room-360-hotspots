3af434294745eb4778691612488901c5a10f50eb
vitoloper <vitoloper@users.noreply.github.com>
2019-10-25 15:47:29 +0200

    Test conversion xy coordinates on texture to 3d world coordinates

--- js/roomHotspots.js
+++ js/roomHotspots.js
@@ -10,6 +10,89 @@ var circle1, circle2, circle3;
 
 var texture;
 
+
+/********************************************************/
+
+
+// Recursively traverse through the model.
+var traversePolygonsForGeometries = function (node, uvx, uvy) {
+  if (node.geometry) {
+      // Return a list of triangles that have the point within them.
+      // The returned objects will have the x,y,z barycentric coordinates of the point inside the respective triangle
+      // TODO: bug here (baryData is null for uvx= 0 uvy=0)
+      var baryData = annotationTest(uvx, uvy, node.geometry.faceVertexUvs);
+      if (baryData.length) {
+          for (var j = 0; j < baryData.length; j++) {
+              // Find the vertices corresponding to the triangle in the model
+              var vertexa = node.geometry.vertices[node.geometry.faces[baryData[j][0]].a];
+              var vertexb = node.geometry.vertices[node.geometry.faces[baryData[j][0]].b];
+              var vertexc = node.geometry.vertices[node.geometry.faces[baryData[j][0]].c];
+              // Sum the barycentric coordinates and apply to the vertices to get the coordinate in local space
+              var worldX = vertexa.x * baryData[j][1] + vertexb.x * baryData[j][2] + vertexc.x * baryData[j][3];
+              var worldY = vertexa.y * baryData[j][1] + vertexb.y * baryData[j][2] + vertexc.y * baryData[j][3];
+              var worldZ = vertexa.z * baryData[j][1] + vertexb.z * baryData[j][2] + vertexc.z * baryData[j][3];
+              var vector = new THREE.Vector3(worldX, worldY, worldZ);
+              // Translate to world space
+              var worldVector = vector.applyMatrix4(node.matrixWorld);
+              return worldVector;              
+          }
+      }
+  }
+
+  if (node.children) {
+      for (var i = 0; i < node.children.length; i++) {
+          var worldVectorPoint = traversePolygonsForGeometries(node.children[i], uvx, uvy);
+          if (worldVectorPoint) return worldVectorPoint;
+      }
+  }
+  };
+  
+  // Loops through each face vertex UV item and tests if it is within the triangle.
+  function annotationTest(uvX, uvY, faceVertexUvArray) {
+      var point = {};
+      point.x = uvX;
+      point.y = uvY;
+      var results = [];
+      for (i = 0; i < faceVertexUvArray[0].length; i++) {
+          var result = ptInTriangle(point, faceVertexUvArray[0][i][0], faceVertexUvArray[0][i][1], faceVertexUvArray[0][i][2]);
+          if (result.length) {
+              results.push([i, result[0], result[1], result[2]]);
+          }
+      }
+      return results;
+  };
+  
+  // This is a standard barycentric coordinate function.
+  function ptInTriangle(p, p0, p1, p2) {
+      var x0 = p.x;
+      var y0 = p.y;
+      
+      var x1 = p0.x;
+      var y1 = p0.y;
+      var x2 = p1.x;
+      var y2 = p1.y;
+      var x3 = p2.x;
+      var y3 = p2.y;
+  
+      var b0 = (x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1)
+      var b1 = ((x2 - x0) * (y3 - y0) - (x3 - x0) * (y2 - y0)) / b0
+      var b2 = ((x3 - x0) * (y1 - y0) - (x1 - x0) * (y3 - y0)) / b0
+      var b3 = ((x1 - x0) * (y2 - y0) - (x2 - x0) * (y1 - y0)) / b0
+  
+      if (b1 > 0 && b2 > 0 && b3 > 0) {
+        console.log(b1, b2, b3);
+          return [b1, b2, b3];
+      } else {
+          return [];
+      }
+  };
+
+
+
+/********************************************************/
+
+
+
 init();
 animate();
 
@@ -42,7 +125,8 @@ function init () {
   // scene.add(axesHelper);
 
   // Main sphere setup (radius, texture)
-  scene.add(mainSphere(radius, texture));
+  var sceneSphere = mainSphere(radius, texture);
+  scene.add(sceneSphere);
 
   // THREE.js
   // Spherical( radius, phi, theta )
@@ -98,7 +182,33 @@ function init () {
 
   window.addEventListener('mousemove', onMouseMove, false);
   window.addEventListener('resize', onWindowResize, false);
+
+  /**
+   * Experiment here: from x y coordinates on executiveroom.jpg to 3d world coordinates.
+   * 
+   */
+
+  // Original image size
+  var textureSize = {width: 2464, height: 1232};
+  
+  // Now, suppose we have a point in x-y coordinates on the image (it's in the center of the pillow on the bed)
+  var imgPoint = {x: 561, y: 709};
+
+  // Get UV coordinates (NOTE: there is a flip on the y coordinates!)
+  var imgPointUV = {u: imgPoint.x/textureSize.width, v: Math.abs(imgPoint.y/textureSize.height - 1)};
+  console.log('Image point UV coordinates: ', imgPointUV);
+
+  // var worldCoordsPoint = traversePolygonsForGeometries(sceneSphere, 0, 0);
+  var worldCoordsPoint = traversePolygonsForGeometries(sceneSphere, imgPointUV.u, imgPointUV.v);
+  console.log('world coordinates point: ', worldCoordsPoint);
+
+  circleP = createHotspot(circleShape, null, 0xff0000, worldCoordsPoint.x, worldCoordsPoint.y, worldCoordsPoint.z, 0, 0, 0, radius/20);
+  circleP.lookAt(0, 0, 0);
+  circleP.userData.HTMLDescription = 'From UV coords';
+  scene.add(circleP);
+
 } // init ()
+
 
 function animate () {
   requestAnimationFrame(animate);
